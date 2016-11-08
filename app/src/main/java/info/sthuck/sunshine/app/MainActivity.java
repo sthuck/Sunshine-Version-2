0fc4dbfc1b205f94157ab8792a0f18b58b85e4be
Aviad Hadad <aviadh@wix.com>
2016-11-08 15:25:19 +0200

    WIP

--- app/src/main/java/info/sthuck/sunshine/app/MainActivity.java
+++ app/src/main/java/info/sthuck/sunshine/app/MainActivity.java
@@ -8,6 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -56,7 +61,26 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            String[] data = {
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11",
+                    "Fri 6/27 - Foggy - 21/10",
+                    "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                    "Sun 6/29 - Sunny - 20/7",
+                    "Mon 6/23 - Sunny - 31/17",
+                    "Tue 6/24 - Foggy - 21/8",
+                    "Wed 6/25 - Cloudy - 22/17",
+                    "Thurs 6/26 - Rainy - 18/11"
+
+            };
+            List<String> weekPrediction = Arrays.asList(data);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekPrediction);
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView lv = (ListView) rootView.findViewById(R.id.listview_forecast);
+            lv.setAdapter(adapter);
             return rootView;
         }
     }
